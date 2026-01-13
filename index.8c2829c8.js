bdab6abf5d1a633705b66870dce7833560778e10
Yaroslav Olieniev <olieniev.od.ua@gmail.com>
2026-01-13 15:53:11 +0100

    make build

---
+++ index.8c2829c8.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".logo"),t=new Promise(function(t){e.addEventListener("click",function(){t()})}),n=new Promise(function(e,t){setTimeout(function(){t(Error())},3e3)});t.then(function(){var e=document.createElement("div");e.className="message",e.textContent="Promise was resolved!",document.body.append(e)}),n.catch(function(){var e=document.createElement("div");e.className="message error-message",e.textContent="Promise was rejected!",document.body.append(e)});
+//# sourceMappingURL=index.8c2829c8.js.map
